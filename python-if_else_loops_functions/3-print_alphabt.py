31526c2d6d9f0bc544ad56dc587bc6d67b8aad7f
abbasova05 <tahminabbasova05@gmail.com>
2025-04-29 15:15:57 +0000

    njgbjgb

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
-#!/usr/bin/python3
-print("". join(chr(i) for i in range(97, 123) if chr(i) != 'q' and chr(i) != 'e'), end=" ")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
