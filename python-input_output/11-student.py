a07ef2c9174f9f7cb0f145913c8205ae1b0e761b
abbasova05 <tahminabbasova05@gmail.com>
2025-06-04 16:35:32 +0000

    tack

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
+"""Student class that can be serialized to and deserialized from JSON."""
+
 class Student:
     def __init__(self, first_name, last_name, age):
+        """Initialize Student with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """
+        Return the dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only those attributes are returned.
+        Otherwise, return all attributes.
+        """
         if attrs is None:
-            # Return all attributes
             return self.__dict__.copy()
         else:
-            # Return only attributes listed in attrs
             new_dict = {}
             for attr in attrs:
                 if hasattr(self, attr):
@@ -18,6 +25,11 @@ class Student:
             return new_dict
 
     def reload_from_json(self, json):
-        # Update all attributes from json dictionary
+        """
+        Replace all attributes of the Student instance with the values in json.
+
+        Args:
+            json (dict): Dictionary with keys as attribute names and values as new attribute values.
+        """
         for key, value in json.items():
-            setattr(self, key, value)   
+            setattr(self, key, value) 
