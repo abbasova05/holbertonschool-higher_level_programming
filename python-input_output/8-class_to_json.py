dc21f0905f98eca590645377249119f8fb94112b
abbasova05 <tahminabbasova05@gmail.com>
2025-06-04 12:28:16 +0000

    tack

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-"""Function that returns the dictionary description for JSON serialization of an object."""
+"""Module that defines a function to retrieve the dictionary
+representation of a class instance for JSON serialization.
+"""
 
 
 def class_to_json(obj):
-    """Return the dictionary representation of a simple data structure."""
+    """Return the dictionary description with simple data structure
+    (list, dictionary, string, integer, and boolean) for JSON serialization
+    of an object.
+
+    Args:
+        obj (object): An instance of a class whose attributes are JSON serializable.
+
+    Returns:
+        dict: The dictionary representation of obj's attributes.
+    """
     return obj.__dict__
