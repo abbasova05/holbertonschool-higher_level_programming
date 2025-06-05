f615e6d66d62818615694e23555381a39b4d46c0
abbasova05 <tahminabbasova05@gmail.com>
2025-06-05 07:51:58 +0000

    tack

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
 """
-This module defines a Student class with serialization
-and deserialization methods for JSON representation.
+This module defines a Student class.
+It includes methods for serializing and deserializing instances to and from JSON format.
 """
 
 
 class Student:
-    """Defines a student by first name, last name, and age"""
+    """Represents a student with first name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance with first name, last name, and age"""
+        """Initializes a Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """
-        Retrieves a dictionary representation of a Student instance.
-        If attrs is a list of strings, only attribute names contained
-        in this list are retrieved.
+        Returns a dictionary representation of a Student instance.
+        If attrs is a list of strings, only attribute names contained in this list are retrieved.
         """
-        if type(attrs) == list and all(type(attr) == str for attr in attrs):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {
+                key: getattr(self, key)
+                for key in attrs if hasattr(self, key)
+            }
         return self.__dict__
 
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance with those in the json dictionary.
+        Replaces all attributes of the Student instance based on a provided dictionary.
         """
         for key, value in json.items():
             setattr(self, key, value)
