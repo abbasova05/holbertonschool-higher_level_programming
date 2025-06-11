61f368a07d42e0efd3480a30e9b75e62c78800dc
abbasova05 <tahminabbasova05@gmail.com>
2025-06-11 14:34:05 +0400

    tack

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,28 +1,33 @@
-#!/usr/bin/python3
 import pickle
-"""Define a class Square."""
-
 
 class CustomObject:
-    """sadsfasdfasfasdfasfad"""
-    def __init__(self, **kwargs):
-        """Initialize Square with size attribute"""
-        self.name = kwargs["name"]
-        self.age = kwargs["age"]
-        self.is_student = kwargs["is_student"]
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
 
     def display(self):
-        """Initialize Square with size attribute"""
-        print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """Initialize Square with size attribute"""
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except Exception as e:
+            print(f"Serialization failed: {e}")
 
     @classmethod
     def deserialize(cls, filename):
-        """Initialize Square with size attribute"""
-        with open(filename, "rb") as f:
-            data = pickle.load(f)
-            return data
+        try:
+            with open(filename, 'rb') as file:
+                obj = pickle.load(file)
+                if isinstance(obj, cls):
+                    return obj
+                else:
+                    print("Deserialized object is not of type CustomObject.")
+                    return None
+        except (FileNotFoundError, pickle.PickleError, EOFError, Exception) as e:
+            print(f"Deserialization failed: {e}")
+            return None
