cdbe40510eb198f27f3842cc65a8585df4866748
abbasova05 <tahminabbasova05@gmail.com>
2025-07-23 20:53:27 +0400

    task

--- restful-api/task14.py
+++ restful-api/task14.py
@@ -1,9 +1,19 @@
-from flask import Flask, request
+from flask import Flask, 
 
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return '''
-    <h1 style="text-align: center;"Sade blog</h1>
-    <a href= "/blog">Bloga kec</a>
-'''
+    <h1> Esas sehife</h1>
+    <a href="/esas_melumat">melumat</a><br>
+    <a href="/digermelumatlar">elave</a><br>
+    '''
+
+@app.route("/esas_melumat", method =["GET","POST"])
+def melumat():
+    melumat = ""
+    if request.method == "GET":
+        return
+
+
